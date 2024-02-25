b29dcbbec5892419b6e6e214709ca4c34f7fc80a
Leelaprasad001 <leelaprasad1607@gmail.com>
2024-02-25 11:25:13 +0530

    expressjs

---
+++ Express + NodeJS/Express Basics/final/1-http-basics.js
@@ -0,0 +1,35 @@
+// const http = require('http')
+
+// const server = http.createServer((req,res)=>{
+//     console.log('user hit the server');
+//     res.end('Hello World')
+// })
+
+// server.listen(3000)
+
+
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  const url = req.url
+  // home page
+  if (url === '/') {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    res.write('<h1>home page</h1>')
+    res.end()
+  }
+  // about page
+  else if (url === '/about') {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    res.write('<h1>about page</h1>')
+    res.end()
+  }
+  // 404
+  else {
+    res.writeHead(404, { 'content-type': 'text/html' })
+    res.write('<h1>page not found</h1>')
+    res.end()
+  }
+})
+
+server.listen(3000)
