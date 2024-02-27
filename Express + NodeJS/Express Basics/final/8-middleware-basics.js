9e1afbd643b5fbb61cf83fd619a1eebb569e5a19
Leelaprasad001 <leelaprasad1607@gmail.com>
2024-02-27 19:49:08 +0530

    MERN Basics covered

---
+++ Express + NodeJS/Express Basics/final/8-middleware-basics.js
@@ -0,0 +1,26 @@
+//A middleware is a  function that takes in three arguments: the request object, the response object and the next function.
+
+
+const express = require('express')
+const app = express()
+
+//  req => middleware => res
+
+const logger = (req, res, next) => {
+  const method = req.method
+  const url = req.url
+  const time = new Date().getFullYear()
+  console.log(method, url, time)
+  next()
+}
+
+app.get('/', logger, (req, res) => {
+  res.send('Home')
+})
+app.get('/about', logger, (req, res) => {
+  res.send('About')
+})
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000....')
+})
