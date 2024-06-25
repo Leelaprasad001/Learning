f7f7025a7edfe0fbc74a1ab20c61142959816f50
Leelaprasad001 <leelaprasad1607@gmail.com>
2024-06-25 19:46:57 +0530

    Mern authenication with email, JWT access & refresh Token

---
+++ MERN Auth/frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import { Route, Routes, useNavigate } from "react-router-dom";
+import AppContainer from "./components/AppContainer";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login"
+import Register from "./pages/Register";
+import Settings from "./pages/Settings";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import VerifyEmail from "./pages/VerifyEmail";
+import { setNavigate } from "./lib/navigation";
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+
+function App() {
+  // set the navigate function on our API client for use in the axios error interceptor
+  // this allows us to redirect to the login page when an auth error occurs
+  const navigate = useNavigate();
+  setNavigate(navigate);
+  return (
+    <Routes>
+      <Route path="/" element={<AppContainer />}>
+        <Route index element={<Profile />} />
+        <Route path="settings" element={<Settings />} />
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/email/verify/:code" element={<VerifyEmail />} />
+      <Route path="/password/forgot" element={<ForgotPassword />} />
+      <Route path="/password/reset" element={<ResetPassword />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ MERN Auth/frontend/src/config/apiClient.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import queryClient from "./queryCient";
+import { UNAUTHORIZED } from "../constants/http.mjs";
+import { navigate } from "../lib/navigation";
+
+const options = {
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+};
+
+// create a separate client for refreshing the access token
+// to avoid infinite loops with the error interceptor
+const TokenRefreshClient = axios.create(options);
+TokenRefreshClient.interceptors.response.use((response) => response.data);
+
+const API = axios.create(options);
+
+API.interceptors.response.use(
+  (response) => response.data,
+  async (error) => {
+    const { config, response } = error;
+    const { status, data } = response || {};
+
+    // try to refresh the access token behind the scenes
+    if (status === UNAUTHORIZED && data?.errorCode === "InvalidAccessToken") {
+      try {
+        // refresh the access token, then retry the original request
+        await TokenRefreshClient.get("/auth/refresh");
+        return TokenRefreshClient(config);
+      } catch (error) {
+        // handle refresh errors by clearing the query cache & redirecting to login
+        queryClient.clear();
+        navigate("/login", {
+          state: {
+            redirectUrl: window.location.pathname,
+          },
+        });
+      }
+    }
+
+    return Promise.reject({ status, ...data });
+  }
+);
+
+export default API;
